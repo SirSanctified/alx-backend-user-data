0e2def52dc660272ea79d9db7359d33885418991
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-02 21:12:37 +0200

    Decode base64 bytes to string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic authentication"""
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -18,4 +19,21 @@ class BasicAuth(Auth):
             if authorization_header.split(' ')[0] != 'Basic':
                 return None
             return authorization_header.split(' ')[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """
+        Return the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header and \
+                isinstance(base64_authorization_header, str):
+            try:
+                return base64.b64decode(
+                    base64_authorization_header).decode('utf-8')
+            except Exception:
+                return None
         return None
