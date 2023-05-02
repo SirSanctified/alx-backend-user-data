ddc12067810d52995eac48f185666fba749f5246
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-02 14:20:04 +0200

    Extract base64 part of the Authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 """Basic authentication"""
 
-from api.v1.auth.auth import Auth
+from auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic Authentication implementation"""
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """
+        Return the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header and isinstance(authorization_header, str):
+            if authorization_header.split(' ')[0] != 'Basic':
+                return None
+            return authorization_header.split(' ')[1]
+        return None
