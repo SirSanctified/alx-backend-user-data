1d5ec5a3fe27f97a9deed296a75b9290e481126c
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-02 15:32:56 +0200

    Add missing imports

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -18,7 +20,7 @@ auth_type = os.getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
-    auth = basic_auth.BasicAuth()
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
