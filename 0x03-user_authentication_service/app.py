843a16fd0e6196cdb081a6872ebc955a45643ea6
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-09 15:46:01 +0200

    Add request method and disable strict slashes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,8 +6,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def root(strict_slashes=False):
+@app.route('/', methods=["GET"], strict_slashes=False)
+def root():
     """Return a dummy JSON payload"""
     return jsonify({"message": "Bienvenue"})
 
