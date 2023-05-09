b53d3da15d0857a3f24a881e16de397914042e1b
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-09 17:09:34 +0200

    Refactor password reset handler

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,13 +57,14 @@ def profile():
 @app.route('/reset_password ', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     email = request.form.get("email")
+    reset_token = None
     try:
         reset_token = AUTH.get_reset_password_token(email)
     except ValueError:
         reset_token = None
     if reset_token is None:
         abort(403)
-    return jsonify({"email": email, "reset_token": reset_token}), 200
+    return jsonify({"email": email, "reset_token": reset_token})
 
 
 if __name__ == "__main__":
