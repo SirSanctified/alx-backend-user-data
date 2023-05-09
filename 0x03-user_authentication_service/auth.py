d5abfbd6e38bae8c80eaffd2498aba1ebae9de50
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-09 16:16:27 +0200

    Create a new session ID for user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,3 +44,13 @@ class Auth:
         except Exception:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """Create a session ID for user and return it"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
