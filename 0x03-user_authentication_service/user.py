2684d86eab0ac1759998c9acf798972c99b64faf
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-09 14:50:03 +0200

    Add missing import in db.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,10 +10,10 @@ Base = declarative_base()
 
 
 class User(Base):
-	__tablename__ = 'users'
+    __tablename__ = 'users'
 
-	id = Column(Integer, primary_key=True)
-	email = Column(String, nullable=False)
-	hashed_password = Column(String, nullable=False)
-	session_id = Column(String, nullable=True)
-	reset_token = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
